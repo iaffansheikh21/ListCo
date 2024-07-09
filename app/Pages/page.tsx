7b0fe2226462c5d7a02af90b0a8f41f9c28b327a
Muhammad Affan Sheikh <muhammadaffansheikh21@gmail.com>
2024-07-09 13:44:12 +0500

    Listing Details Pages

---
+++ app/Pages/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/NavBar'
+
+export const page_listing = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+export default page_listing
