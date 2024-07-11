a2e1d1af9ba95e8d4a27cfde1fa8d7308546bad8
iaffansheikh21 <muhammadaffansheikh21@gmail.com>
2024-07-11 11:39:47 +0500

    modifications

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // postcss.config.mjs
 export default {
   plugins: {
@@ -6,13 +5,3 @@ export default {
     autoprefixer: {},
   },
 };
-=======
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
->>>>>>> 5340a32f81c9b911159b062001850939954ae21d
