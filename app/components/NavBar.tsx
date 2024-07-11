1e6a8917dc9fa3fc316288f68386b1694f3df6fa
iaffansheikh21 <muhammadaffansheikh21@gmail.com>
2024-07-11 14:40:18 +0500

    responsivess added in navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,14 @@
-"use client";
+'use client'
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSignInAlt,faSearch,faCaretDown,faBars} from "@fortawesome/free-solid-svg-icons";
+import {
+  faSignInAlt,
+  faSearch,
+  faCaretDown,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar: React.FC = () => {
   const [isBlogOpen, setBlogOpen] = useState(false);
@@ -34,17 +39,6 @@ const Navbar: React.FC = () => {
   const toggleCategories = () => {
     setShowCategories(!showCategories);
   };
-
-  const toggleBlogMenu = () => {
-    setBlogOpen(!isBlogOpen);
-    if (isPagesOpen) setPagesOpen(false);
-  };
-
-  const togglePagesMenu = () => {
-    setPagesOpen(!isPagesOpen);
-    if (isBlogOpen) setBlogOpen(false);
-  };
-
   const toggleLanguages = () => {
     setShowLanguages(!showLanguages);
   };
@@ -57,14 +51,26 @@ const Navbar: React.FC = () => {
   return (
     <div className="relative">
       <nav
-        className={`fixed top-0 left-0 w-full z-50 flex flex-col md:flex-row justify-between items-center py-4 px-8 text-white transition-all duration-300 ${
+        className={`fixed top-0 left-0 w-full z-50 flex justify-between items-center py-4 px-8 text-white transition-all duration-300 ${
           isScrolled ? "bg-slate-800" : "bg-transparent"
         }`}
       >
-        <div className="mb-4 md:mb-0 flex-shrink-0">
+        <div className="flex items-center">
           <Image src="/logo.svg" alt="logo" width={140} height={140} />
         </div>
-        <div className="flex md:hidden">
+
+        <div className="hidden md:flex md:items-center space-x-4">
+          <Link href="/" passHref>
+            <span className="hover:text-purple-300 cursor-pointer">Home</span>
+          </Link>
+          <Link href="/categories" passHref>
+            <span className="hover:text-purple-300 cursor-pointer">
+              Categories
+            </span>
+          </Link>
+        </div>
+
+        <div className="flex items-center md:hidden">
           <button
             onClick={toggleMenu}
             className="text-white focus:outline-none"
@@ -72,92 +78,15 @@ const Navbar: React.FC = () => {
             <FontAwesomeIcon icon={faBars} size="lg" />
           </button>
         </div>
-        <div
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } md:flex md:items-center justify-center space-y-4 md:space-y-0 md:space-x-4 mt-4 md:mt-0 w-full md:w-auto`}
-        >
-          <Link href="/" passHref>
-            <span className="hover:text-purple-300 cursor-pointer">Home</span>
-          </Link>
-          <Link href="/Categories" passHref>
-            <span className="hover:text-purple-300 cursor-pointer">
-              Categories
-            </span>
-          </Link>
-          {/* <div className="relative">
-            <span
-              className="hover:text-purple-300 cursor-pointer"
-              onClick={togglePagesMenu}
-            >
-              Pages
-            </span>
-            {isPagesOpen && (
-              <div
-                className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg"
-                onMouseEnter={() => setPagesOpen(true)}
-                onMouseLeave={() => setPagesOpen(false)}
-              >
-                <Link href="/Pages" passHref>
-                  <span className="block px-4 py-2 hover:text-purple-300 cursor-pointer">
-                    Page Listing
-                  </span>
-                </Link>
-                <Link href="/Categories" passHref>
-                  <span className="block px-4 py-2 hover:text-purple-300 cursor-pointer">
-                    Categories
-                  </span>
-                </Link>
-              </div>
-            )}
-          </div> */}
-          {/* <div className="relative">
-            <span
-              className="hover:text-purple-300 cursor-pointer"
-              onClick={toggleBlogMenu}
-            >
-              Blog
-            </span>
-            {isBlogOpen && (
-              <div
-                className="absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg"
-                onMouseEnter={() => setBlogOpen(true)}
-                onMouseLeave={() => setBlogOpen(false)}
-              >
-                <Link href="/blog" passHref>
-                  <span className="block px-4 py-2 hover:text-purple-300 cursor-pointer">
-                    Blog
-                  </span>
-                </Link>
-                <Link href="/blog-menu" passHref>
-                  <span className="block px-4 py-2 hover:text-purple-300 cursor-pointer">
-                    Blog Menu
-                  </span>
-                </Link>
-                <Link href="/elements" passHref>
-                  <span className="block px-4 py-2 hover:text-purple-300 cursor-pointer">
-                    Elements
-                  </span>
-                </Link>
-              </div>
-            )}
-          </div> */}
-          {/* <Link href="/contact" passHref>
-            <span className="hover:text-purple-300 cursor-pointer">Contact</span>
-          </Link> */}
-        </div>
-        <div className="flex space-x-4 mt-4 md:mt-0">
-          {/* <button className="bg-purple-600 hover:bg-purple-700 transform transition-transform duration-500 text-white py-2 px-4 md:px-6 rounded-md text-sm md:text-base">
-            Add Listing
-          </button> */}
-          <button className="bg-purple-600 hover:bg-purple-700 transform transition-transform duration-500 text-white py-2 px-4 md:px-6 rounded-md flex items-center text-sm md:text-base">
+        <div className="hidden md:flex items-center space-x-4">
+          <button className="bg-purple-600 hover:bg-purple-700 transform transition-transform duration-500 text-white py-2 px-4 rounded-md flex items-center text-sm md:text-base">
             <FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
             Log In
           </button>
           <div className="relative">
             <button
               onClick={toggleLanguages}
-              className="hover:bg-purple-700 text-black transform transition-transform duration-500 py-2 px-4 md:px-6 rounded-md flex items-center text-sm md:text-base bg-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-purple-600 hover:text-white"
+              className="hover:bg-purple-700 text-black transform transition-transform duration-500 py-2 px-4 rounded-md flex items-center text-sm md:text-base bg-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-purple-600 hover:text-white"
             >
               <span className="hidden md:inline">{selectedLanguage}</span>
               <FontAwesomeIcon icon={faCaretDown} className="ml-2" />
@@ -181,6 +110,36 @@ const Navbar: React.FC = () => {
           </div>
         </div>
       </nav>
+
+      {/* Sidebar Menu for Small Screens */}
+      <div
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } md:hidden fixed inset-0 bg-gray-900 bg-opacity-95 z-50 flex flex-col items-center space-y-4 py-8`}
+      >
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none self-end mr-8"
+        >
+          <FontAwesomeIcon icon={faBars} size="lg" />
+        </button>
+        <Link href="/" passHref>
+          <span
+            onClick={toggleMenu}
+            className="block py-2 px-4 md:px-6 hover:text-purple-300 cursor-pointer"
+          >
+            Home
+          </span>
+        </Link>
+        <Link href="/categories" passHref>
+          <span
+            onClick={toggleMenu}
+            className="block py-2 px-4 md:px-6 hover:text-purple-300 cursor-pointer"
+          >
+            Categories
+          </span>
+        </Link>
+      </div>
 
       <section
         className="flex flex-col justify-center items-center h-96 bg-cover bg-center relative"
