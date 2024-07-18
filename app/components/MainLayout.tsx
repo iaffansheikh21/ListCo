344c1e0ca88fb91c1ae8536e5de149207bd47b98
iaffansheikh21 <muhammadaffansheikh21@gmail.com>
2024-07-18 10:34:23 +0500

    Spacing issues in Listing page resolved

--- app/components/MainLayout.tsx
+++ app/components/MainLayout.tsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import DescriptionPic from './DescriptionPic';
-// import ContactForm from './ContactForm';
-// const MainLayout: React.FC = () => {
-//   return (
-//     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row md:space-x-8 items-start">
-//       <div className="md:w-8/12 w-full">
-//         <DescriptionPic />
-//       </div>
-//       <div className="md:w-4/12 w-full mt-8 md:mt-0">
-//         <ContactForm />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MainLayout;
-
 import React from 'react';
 import DescriptionPic from './DescriptionPic';
 import ContactForm from './ContactForm';
